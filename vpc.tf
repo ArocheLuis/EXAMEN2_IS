4b122b71e6a0889f3eca78f2b79bda8247feec75
ArocheLuis <79294051+ArocheLuis@users.noreply.github.com>
2023-05-06 17:25:01 -0600

    arreglar bug
    codigo para arreglar el problema, donde no creaba las vpc, subnet, tablas. ERA PORQUE ESTABA EN LA REGION 1 Y ERA EN LA REGION 2 (US-EAST-2)

--- vpc.tf
+++ vpc.tf
@@ -1,48 +1,48 @@
 provider "aws" {
-  region     = "us-east-1"
+  region     = "us-east-2"
 }
 
-resource "aws_vpc" "my_vpc_examen" {
+resource "aws_vpc" "my_vpc" {
   cidr_block = "172.16.0.0/16"
 }
 
-resource "aws_subnet" "public_subnet_examen" {
-  vpc_id     = aws_vpc.my_vpc_examen.id
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
   cidr_block = "172.16.0.0/24"
 }
 
-resource "aws_subnet" "private_subnet_examen" {
-  vpc_id     = aws_vpc.my_vpc_examen.id
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
   cidr_block = "172.16.1.0/24"
 }
 
-resource "aws_internet_gateway" "my_igw_examen" {
-  vpc_id = aws_vpc.my_vpc_examen.id
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
 }
 
-resource "aws_route_table" "public_route_table_examen" {
-  vpc_id = aws_vpc.my_vpc_examen.id
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.my_igw_examen.id
+    gateway_id = aws_internet_gateway.my_igw.id
   }
 }
 
 resource "aws_route_table_association" "public_subnet_association" {
-  subnet_id      = aws_subnet.public_subnet_examen.id
-  route_table_id = aws_route_table.public_route_table_examen.id
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
 }
 
 resource "aws_nat_gateway" "my_nat_gateway" {
   allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public_subnet_examen.id
+  subnet_id     = aws_subnet.public_subnet.id
 }
 
 resource "aws_eip" "nat_eip" {}
 
 resource "aws_route_table" "private_route_table" {
-  vpc_id = aws_vpc.my_vpc_examen.id
+  vpc_id = aws_vpc.my_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -51,6 +51,6 @@ resource "aws_route_table" "private_route_table" {
 }
 
 resource "aws_route_table_association" "private_subnet_association" {
-  subnet_id      = aws_subnet.private_subnet_examen.id
-  route_table_id = aws_route_table.private_route_table_.id
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_route_table.id
 }
